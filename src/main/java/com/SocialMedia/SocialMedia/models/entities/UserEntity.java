52b5b920f7a0b7021282b636908c9f02321f0eda
NechiforAdrian <nechifor.adrian96@yahoo.com>
2023-04-30 21:36:27 +0300

    Create new class UserEntity, Add for this class annotation @Data.

--- src/main/java/com/SocialMedia/SocialMedia/models/entities/UserEntity.java
+++ src/main/java/com/SocialMedia/SocialMedia/models/entities/UserEntity.java
@@ -1,4 +1,22 @@
 package com.SocialMedia.SocialMedia.models.entities;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UserEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String userPassword;
+    private LocalDate createdAt;
+    private LocalDate birthday;
+    private String address;
+    private String phoneNumber;
+    private int nrOfFollowers;
+    private int nrFollowing;
+
 }
