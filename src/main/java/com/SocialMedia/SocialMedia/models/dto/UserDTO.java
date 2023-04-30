b434d239849a3d9bea04eba0477e6e473c7e947f
Cristi <cristi6891@yahoo.com>
2023-04-30 17:39:47 +0200

    feature/new-user-Dto

--- src/main/java/com/SocialMedia/SocialMedia/models/dto/UserDTO.java
+++ src/main/java/com/SocialMedia/SocialMedia/models/dto/UserDTO.java
@@ -1,4 +1,22 @@
 package com.SocialMedia.SocialMedia.models.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+
 public class UserDTO {
+
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String username;
+    public LocalDate birthday;
+    public String address;
+    public String phoneNumber;
+    public int nrOfFollowers;
+    public int nrFollowing;
+
 }
